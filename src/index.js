6be8d68e5fd4e8c692a118a18fe45156cec942c2
roundRekt <roundrekt@icloud.com>
2026-01-17 17:33:45 +0800

    添加弹窗提示功能

--- src/index.js
+++ src/index.js
@@ -6,26 +6,48 @@ export default {
         const key = pathparts.join("/");
         const indexPath = "prod/index.json";
         const patchPath = "prod/patch.json";
+        const statusKey = "status.json";
         const headers = new Headers({ "Content-Type": "application/json; charset=utf-8" });
 
         if (pathname === indexPath || pathname === patchPath) return new Response(await env.NOTICEINDEX.get(pathname), { headers });
         else if (key === indexPath) {
             const index = JSON.parse(await env.NOTICEINDEX.get(indexPath));
             const patch = JSON.parse(await env.NOTICEINDEX.get(patchPath));
+            const status = JSON.parse(await env.RESOURCESTATUS.get(statusKey));
             const noticeindex = Object.fromEntries(
                 Object.keys({ ...index, ...patch }).map(k =>
                     [k, Array.isArray(index[k]) && Array.isArray(patch[k]) ? [...index[k], ...patch[k]] : patch[k] ?? index[k]]
                 )
             );
-            const notice = {
+            const dash = {
                 "NoticeId": 0,
                 "StartDate": "2026-01-01T00:00:00",
-                "EndDate": "2099-12-31T00:00:00",
+                "EndDate": "2099-12-31T23:59:59",
                 "Url": `https://prod-notice.bluearchive.cafe/prod/bluearchive-cafe/dash/index.html?uuid=${uuid}`,
                 "Title": "汉化偏好设置",
                 "DisplayOrder": 0
             };
-            noticeindex.Issues.push(notice);
+            const popup = {
+                "GuidePopupId": 0,
+                "GuidePopupType": 1,
+                "PopupType": 2,
+                "StartDate": "2026-01-01T00:00:00",
+                "EndDate": "2099-12-31T23:59:59",
+                "FileName": "banner.png",
+                "Url": "https://prod-notice.bluearchive.cafe/prod/bluearchive-cafe/popup/",
+                "Message": "　因版本更新，部分汉化功能暂时失效　请老师耐心等待修复完成！",
+                "SurveyId": 0,
+                "NotifyUrl": null,
+                "GotoUrl": "https://bluearchive.cafe/status",
+                "DisplayOrder": 0,
+                "PopupOKText": "查看"
+            }
+            if (
+                status.table.official.version !== status.table.localized.version ||
+                status.asset.official.version !== status.asset.localized.version ||
+                status.media.official.version !== status.media.localized.version
+            ) noticeindex.GuidePopup.push(popup);
+            noticeindex.Issues.push(dash);
             return new Response(JSON.stringify(noticeindex, null, 2), { headers });
         }
 
